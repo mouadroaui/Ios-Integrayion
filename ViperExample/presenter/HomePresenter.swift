1913b9d7013319826c7f14efabbd2971f86be5a0
mouad raoui <m.raoui@futuretrendz.com>
2019-08-30 17:07:13 +0100

    first commit

---
+++ ViperExample/presenter/HomePresenter.swift
@@ -0,0 +1,13 @@
+//
+//  HomePresenter.swift
+//  ViperExample
+//
+//  Created by raoui mouad on 8/30/19.
+//  Copyright © 2019 raoui mouad. All rights reserved.
+//
+
+import UIKit
+
+class HomePresenter: Class {
+
+}
---
+++ ViperExample/protocol/HomeProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  HomeProtocol.swift
+//  ViperExample
+//
+//  Created by raoui mouad on 8/30/19.
+//  Copyright © 2019 raoui mouad. All rights reserved.
+//
+
+import UIKit
+
+class HomeProtocol: Class {
+
+}
